c3ccfc10d6773d57bdc66a2fbb5da1a3ac095451
SarahKatherineFrancis <sarah.katherinefrancis@gmail.com>
2021-12-14 20:06:18 +0800

    Added HTML, JavaScript and CSS Files

---
+++ src/index.js
@@ -0,0 +1,86 @@
+let now = new Date();
+let h6 = document.querySelector("h6");
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+h6.innerHTML = `${day} ${hours}:${minutes}`;
+let form = document.querySelector("#form");
+form.addEventListener("submit", search);
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+}
+function convertToFarenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = 64;
+}
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature-value");
+  temperatureElement.innerHTML = 18;
+}
+let temperature = document.querySelector("#temperature-value");
+let farenheitLink = document.querySelector("#farenheit-temperature");
+farenheitLink.addEventListener("click", convertToFarenheit);
+
+let celsiusLink = document.querySelector("#celsius-temperature");
+celsiusLink.addEventListener("click", convertToCelsius);
+let searchInput = document.querySelector("#search-city-input");
+function displayWeatherCondition(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature-value").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "8e7ae886bfd9f66febdffcb5fb779942";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#form");
+searchForm.addEventListener("submit", handleSubmit);
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function searchLocation(position) {
+  let apiKey = "8e7ae886bfd9f66febdffcb5fb779942";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+searchCity("Cape Town");
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
